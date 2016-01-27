8dcec0bc57a42e47184989176d430ec7f06b296c
duhaime <douglas.duhaime@gmail.com>
2016-01-27 08:08:19 -0500

    add route for show method of home controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Chalkboard::Application.routes.draw do
-  get "home/show"
-  root 'home#show'
-
+  root "home#show"
+  get "home/show" => "home#show", as: "home"
+ 
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
