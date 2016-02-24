7585dcfcc44e8103bcfbe714ee8952d5434e2f99
duhaime <douglas.duhaime@gmail.com>
2016-02-23 19:17:16 -0500

    only index the first 100 records during development, and strip quotation marks on db ingest

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,19 +11,22 @@ estc_path = File.join(Rails.root, "db", "estc/estc_streamlined_bib_data.tsv")
 
 estc_records = File.open(estc_path,'r:UTF-8',&:read).split("\n")
 
-estc_records.each do |record|
-  rs = record.split("\t")
+estc_records.each_with_index do |record, record_index|
+  if record_index < 100
 
-  # create a new db row for this record
-  new_book = Book.new(:estc_cit_number => rs[0], :author_name => rs[1],
-    :author_titles_etc => rs[2], :author_dates => rs[3],
-    :corporate_author => rs[4], :corporate_author_specific => rs[5],
-    :main_title => rs[6], :continuation_of_title => rs[7],
-    :publication_place => rs[8], :imprint => rs[9],
-    :imprint_date => rs[10], :pagination => rs[11], 
-    :illustrations => rs[12], :format => rs[13], 
-    :pub_year => rs[14], :bibliographic_citations => rs[15])
+    rs = record.gsub('"', '').split("\t")
 
-  # save the record
-  new_book.save!
+    # create a new db row for this record
+    new_book = Book.new(:estc_cit_number => rs[0], :author_name => rs[1],
+      :author_titles_etc => rs[2], :author_dates => rs[3],
+      :corporate_author => rs[4], :corporate_author_specific => rs[5],
+      :main_title => rs[6], :continuation_of_title => rs[7],
+      :publication_place => rs[8], :imprint => rs[9],
+      :imprint_date => rs[10], :pagination => rs[11], 
+      :illustrations => rs[12], :format => rs[13], 
+      :pub_year => rs[14], :bibliographic_citations => rs[15])
+
+    # save the record
+    new_book.save!
+  end
 end
